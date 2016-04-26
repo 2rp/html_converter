68af02ef08776fd65e475b7c86f2adab0830b736
Søren Torp Petersen <stp@iMac.home>
2016-04-26 09:43:32 +0100

    Add Pry for debugging. Add :focus filter to specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require 'html_converter'
+require 'pry'
 
 RSpec.configure do |config|
   config.color = true
   config.formatter = :documentation
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 end
 
 
