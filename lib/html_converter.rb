baa9f3aa4616e8c234ad4f033a49ecc3b279774c
2rp <stp@iMac.home>
2016-04-26 13:59:22 +0100

    Add #convert_to(format) to API.

--- lib/html_converter.rb
+++ lib/html_converter.rb
@@ -3,10 +3,23 @@ require 'html_converter/to_text'
 require 'html_converter/to_html_blocks'
 
 module HtmlConverter
+
+  CONVERTER = {
+    text: ToText,
+    html_blocks: ToHtmlBlocks
+  }
+
   class Converter
     attr_reader :html
+
     def initialize(html)
       @html = html
+    end
+
+    def convert_to(format)
+      format or raise "Please specify a :format to use #convert."
+      converter = CONVERTER[format] or raise "Couldn't recognize format: '#{format}'."
+      converter.new(self.html).convert
     end
 
     def to_text
--- spec/html_converter_spec.rb
+++ spec/html_converter_spec.rb
@@ -8,7 +8,7 @@ module HtmlConverter
     describe '#to_text' do
       let(:html) { '<p>a <strong>b</strong></p>' }
 
-      it 'converts' do
+      it 'converts html input to text' do
         expect(subject.to_text).to eq('a b')
       end
     end
@@ -16,10 +16,46 @@ module HtmlConverter
     describe '#to_html_blocks' do
       let(:html) { '<p>a</p><p>b <strong>c</strong></p>' }
 
-      it 'converts' do
+      it 'converts html input to blocks of html' do
         expect(subject.to_html_blocks).to eq(['a', '', 'b <strong>c</strong>'])
+      end
+    end
+
+    describe '#convert_to' do
+      let(:html) { '<p>a</p>' }
+      let(:result) { subject.convert_to(format) }
+
+      context "when format: :text" do
+        let(:format) { :text }
+
+        it 'converts html input to text' do
+          expect(result).to eq('a')
+        end
+      end
+
+      context "when format: :html_blocks" do
+        let(:format) { :html_blocks }
+
+        it 'converts html input to html blocks' do
+          expect(result).to eq(['a'])
+        end
+      end
+
+      context "when format is not recognized" do
+        let(:format) { :unrecognized_format }
+
+        it 'raises an error' do
+          expect { result }.to raise_error("Couldn't recognize format: '#{format}'.")
+        end
+      end
+
+      context "when format is blank" do
+        let(:format) { nil }
+
+        it 'raises an error' do
+          expect { result }.to raise_error("Please specify a :format to use #convert.")
+        end
       end
     end
   end
 end
-
